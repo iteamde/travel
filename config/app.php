4ae3c7a6f91fbd1f876e4bdbeefa96dd68d094ca
Mahmoud Hussien <husen@bezaat.com>
2017-06-09 19:46:28 +0300

    modifying tables engine to innodb@

--- config/app.php
+++ config/app.php
@@ -38,7 +38,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -114,7 +114,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => 'hskwjsmsjshskwjsmsjshskwjsmsjssd',
 
     'cipher' => 'AES-256-CBC',
 
