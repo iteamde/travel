fe1ceb294fb534ddd28dcd3a70d00feabfee9580
jawad ijaz <jawad.ijaz@granjur.net>
2017-07-11 19:32:20 +0500

    api middleware update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -47,8 +47,8 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
-            'bindings',
+            // 'throttle:60,1',
+            // 'bindings',
         ],
     ];
 
