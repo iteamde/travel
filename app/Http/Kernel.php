da9372765b1fc043d47405a66b06db43c66ea882
jawad ijaz <jawad.ijaz@granjur.net>
2017-07-13 03:33:49 +0500

    test

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,24 +30,25 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            // \App\Http\Middleware\EncryptCookies::class,
+            // \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
-            \Illuminate\Session\Middleware\AuthenticateSession::class,
+            // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\LocaleMiddleware::class,
+            // \App\Http\Middleware\VerifyCsrfToken::class,
+            // \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            // \App\Http\Middleware\LocaleMiddleware::class,
         ],
 
         'admin' => [
-            'auth',
-            'access.routeNeedsPermission:view-backend',
-            'timeout',
+            // 'auth',
+            // 'access.routeNeedsPermission:view-backend',
+            // 'timeout',
         ],
 
         'api' => [
-            // 'throttle:60,1',
+            // 'access.routeNeedsPermission:view-api',
+            'throttle:60,1',
             // 'bindings',
         ],
     ];
