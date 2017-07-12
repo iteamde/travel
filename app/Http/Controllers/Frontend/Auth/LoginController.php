e558ded43f9120fb3fca548757742bbdb08b6dfa
jawad ijaz <jawad.ijaz@granjur.net>
2017-07-13 03:06:59 +0500

    test

--- app/Http/Controllers/Frontend/Auth/LoginController.php
+++ app/Http/Controllers/Frontend/Auth/LoginController.php
@@ -24,7 +24,7 @@ class LoginController extends Controller
      * @return string
      */
     public function redirectPath()
-    {
+    {   
         if (access()->allow('view-backend')) {
             return route('admin.dashboard');
         }
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -72,6 +72,8 @@ class RouteServiceProvider extends ServiceProvider
         Route::middleware('web')
              ->namespace($this->namespace)
              ->group(base_path('routes/web.php'));
+        // Route::namespace($this->namespace)
+        //      ->group(base_path('routes/web.php'));
     }
 
     /**
