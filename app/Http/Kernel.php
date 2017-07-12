f644f708f46a71010cfbd5a0670ead7a915a487a
jawad ijaz <jawad.ijaz@granjur.net>
2017-07-13 03:44:58 +0500

    auth disabled

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends HttpKernel
     protected $middlewareGroups = [
         'web' => [
             \App\Http\Middleware\EncryptCookies::class,
-            // \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
@@ -41,7 +41,7 @@ class Kernel extends HttpKernel
         ],
 
         'admin' => [
-            'auth',
+            // 'auth',
             'access.routeNeedsPermission:view-backend',
             'timeout',
         ],
