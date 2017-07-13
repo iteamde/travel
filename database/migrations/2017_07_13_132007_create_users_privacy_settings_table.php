a6095c1fa67ed66f4c6187beece94c0ebea5d26c
Mahmoud Hussien <husen@bezaat.com>
2017-07-13 16:51:10 +0300

    add table users_privacy_settings

--- database/migrations/2017_07_13_132007_create_users_privacy_settings_table.php
+++ database/migrations/2017_07_13_132007_create_users_privacy_settings_table.php
@@ -13,7 +13,23 @@ class CreateUsersPrivacySettingsTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('users_notification_settings', function(Blueprint $table) {
+            $sql = 'DROP TABLE IF EXISTS `users_notification_settings`;';
+            DB::connection()->getPdo()->exec($sql);
+
+            $sql = 'CREATE TABLE `users_notification_settings` (
+                    `id` int(11) NOT NULL,
+                    `users_id` int(11) NOT NULL,
+                    `var` varchar(255) NOT NULL,
+                    `val` varchar(255) NOT NULL
+                  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+              ALTER TABLE `users_notification_settings`
+                ADD PRIMARY KEY (`id`),
+                ADD KEY `users_id` (`users_id`);
+              ALTER TABLE `users_notification_settings`
+                MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;';
+            DB::connection()->getPdo()->exec($sql);
+        });
     }
 
     /**
