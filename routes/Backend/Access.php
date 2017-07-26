75067d23954d2618218253de8d5f8e89a97ccc9b
Mahmoud Hussien <husen@bezaat.com>
2017-07-26 16:47:34 +0300

    implemenet detailed admin logs

--- routes/Backend/Access.php
+++ routes/Backend/Access.php
@@ -22,9 +22,7 @@ Route::group([
             Route::post('user/get', 'UserTableController')->name('user.get');
             Route::get('user/logs', 'UserController@getLogs')->name('user.logs');
             Route::post('user/logs', 'UserController@postLogs')->name('user.logs');
-            Route::get('user/logs', 'UserController@getLogs')->name('user.logbyid');
-            Route::post('user/logs', 'UserController@postLogs')->name('user.logsbyid');
-
+            
             /*
              * User Status'
              */
