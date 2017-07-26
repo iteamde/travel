05ca916eb70199073a40a1e7af2a5e8a5d7eb1cc
Mahmoud Hussien <husen@bezaat.com>
2017-07-26 15:22:23 +0300

    admin logs stats

--- resources/views/backend/access/logs.blade.php
+++ resources/views/backend/access/logs.blade.php
@@ -36,7 +36,7 @@
                     @foreach($logs AS $log)
                     <tr>
                         <td>{{ $log->admin_id }}</td>
-                        <th>{{ $log->user->email }}</th>
+                        <th>{{ dd($log->user) }}</th>
                         <th>{{ $log->total }}</th>
                     </tr>
                     @endforeach
