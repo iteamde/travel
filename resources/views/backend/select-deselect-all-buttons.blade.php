72c92a75c2a06394f362e1f56247c19193e106a4
jawad ijaz <jawad.ijaz@granjur.net>
2017-10-03 11:14:16 +0500

    Select all buttons file added

---
+++ resources/views/backend/select-deselect-all-buttons.blade.php
@@ -0,0 +1,13 @@
+<div class="mb-10 hidden-sm hidden-xs">
+    <!-- {{ link_to_route('admin.access.user.create', trans('menus.backend.access.users.create'), [], ['class' => 'btn btn-success btn-xs']) }} -->
+    <a id="select-all" href="javascript:void(0);" class="btn btn-success btn-xs">Select All</a>
+    <a id="deselect-all" href="javascript:void(0);" class="btn btn-danger btn-xs">De-Select All</a>
+
+</div><!--pull right-->
+
+<div class="pull-right mb-10 hidden-sm hidden-xs">
+    <!-- {{ link_to_route('admin.access.user.create', trans('menus.backend.access.users.create'), [], ['class' => 'btn btn-success btn-xs']) }} -->
+    <a id="delete-all-selected" href="javascript:void(0);" class="btn btn-danger btn-xs">Delete All Selected</a>
+</div><!--pull right-->
+
+<div class="clearfix"></div>
