e063daf988bb86083c3f6723fbc954a1727b1230
Mahmoud Hussien <husen@bezaat.com>
2017-10-09 17:17:00 +0300

    implement grap media cron

--- app/Http/Controllers/CronsController.php
+++ app/Http/Controllers/CronsController.php
@@ -3,17 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Models\ActivityMedia\Media;
+use App\Models\Place\PlaceMedias;
 
-class CronsController extends Controller
-{
+class CronsController extends Controller {
 
     public function getPlacesMedia(Request $request) {
         $places_without_media = \App\Models\Place\Place::leftJoin('places_medias', 'places.id', '=', 'places_medias.places_id')
                 ->whereNull('places_medias.places_id')
                 ->orderBy('places.id', 'ASC')
-                ->take(10)
+                ->take(1)
                 ->get();
-        foreach($places_without_media AS $pwm) {
+        foreach ($places_without_media AS $pwm) {
             //var_dump($pwm->provider_id);
             if (time() % 2 == 0) {
                 $json = file_get_contents('http://db.travooo.com/public/places/media/go/' . $pwm->provider_id);
@@ -21,7 +23,27 @@ class CronsController extends Controller
                 $json = file_get_contents('http://db.travooodev.com/public/places/media/go/' . $pwm->provider_id);
             }
 
-            dd($json);
+            $photos = unserialize($json);
+
+            $place = \App\Models\Place\Place::where('provider_id', $pwm->provider_id)->get()->first();
+
+
+            foreach ($photos AS $p) {
+                $media_file = 'places_media/' . $pwm->provider_id . '/' . sha1(microtime()) . '.jpg';
+                Storage::disk('public')->put($media_file, $p);
+
+                $media = new Media;
+                $media->url = $media_file;
+                $media->save();
+
+                $place_media = new PlaceMedias;
+                $place_media->places_id = $place->id;
+                $place_media->medias_id = $media->id;
+                $place_media->save();
+            }
+
+//            var_dump($photos);
         }
     }
+
 }
