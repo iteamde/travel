7d1a5f3891799fa2b8b36d5ecc8e5a6ee6b39c72
Mahmoud Hussien <husen@bezaat.com>
2017-10-15 16:03:45 +0300

    fix edit place page

--- resources/views/backend/place/edit.blade.php
+++ resources/views/backend/place/edit.blade.php
@@ -50,8 +50,7 @@ use App\Models\Access\language\Languages;
 @endsection
 
 @section('content')
-{{var_dump($images)}}
-{{dd($medias)}}
+{{dd($images)}}
     {{ Form::model($place, [
             'id'     => 'place_update_form',
             'route'  => ['admin.location.place.update', $placeid],
