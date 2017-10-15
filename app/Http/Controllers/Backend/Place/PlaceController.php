9e647f2a3c89416142e61f59bd312e769c66a1b8
Mahmoud Hussien <husen@bezaat.com>
2017-10-15 16:47:43 +0300

    fix edit place page

--- app/Http/Controllers/Backend/Place/PlaceController.php
+++ app/Http/Controllers/Backend/Place/PlaceController.php
@@ -294,7 +294,7 @@ class PlaceController extends Controller {
             // if(isset($value->transsingle) && !empty($value->transsingle)){
                 array_push($images_arr,[
                     'id' => $value->id,
-                    'url' => asset(Storage::disk('public')->url($value->url))
+                    'url' => asset(Storage::url($value->url))
                 ]);
 
         }
