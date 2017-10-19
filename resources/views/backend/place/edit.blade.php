00cc2a602eb1821d971a7870e13d37d4a96801f4
Mahmoud Hussien <husen@bezaat.com>
2017-10-19 12:13:04 +0300

    test

--- resources/views/backend/place/edit.blade.php
+++ resources/views/backend/place/edit.blade.php
@@ -326,7 +326,7 @@ use App\Models\Access\language\Languages;
                     @foreach( $images as $image)
                     <li class="col-md-2" style="">
                         <i class="fa fa-times delete-image" data-id="<?= $image['id'] ?>" aria-hidden="true" style="color:red;position: relative;top:20px;left:85px;" ></i>
-                        <img src="<?= $image['url'] ?>" style="width:100px;height:100px;" id="<?= $image['id'] ?>" <?php if($image['id']==$featured_media) { echo 'class="hover"'; } ?> />
+                        <img src="<?= $image['url'] ?>" style="width:100px;height:100px;" id="<?= $image['id'] ?>" <?php if(isset($featured_media) && $image['id']==$featured_media) { echo 'class="hover"'; } ?> />
                     </li>
                     @endforeach
                     </ul>
