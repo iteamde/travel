aa16dafb40db90bb362d451a7c8fa0da4b4590fa
jawad ijaz <jawad.ijaz@granjur.net>
2018-01-04 14:38:00 +0500

    Embassies test case added

--- app/Models/Embassies/Embassies.php
+++ app/Models/Embassies/Embassies.php
@@ -3,8 +3,8 @@
 namespace App\Models\Embassies;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Embassies\Traits\Relationship\EmbassiesRelationship;
-use App\Models\Embassies\Traits\Attribute\EmbassiesAttribute;
+// use App\Models\Embassies\Traits\Relationship\EmbassiesRelationship;
+// use App\Models\Embassies\Traits\Attribute\EmbassiesAttribute;
 
 class Embassies extends Model
 {
@@ -17,8 +17,8 @@ class Embassies extends Model
      */
     protected $table = 'embassies';
     
-    use EmbassiesRelationship,
-        EmbassiesAttribute;
+    // use EmbassiesRelationship,
+    //     EmbassiesAttribute;
 
     public $timestamps = false;
 
