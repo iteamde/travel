f471659cd8071f8f334c84c7444ebee438d709fd
jawad ijaz <jawad.ijaz@granjur.net>
2018-02-01 17:39:56 +0500

    Consumer key change

--- config/config.php
+++ config/config.php
@@ -2,11 +2,11 @@
  
 return [
     //
-    'consumer_key'      => '7OysPrT19Vo59XfV1Pilp9KQx',
-    'consumer_secret'   => 'UI3SDyymSc0pSxL5512ZCRnmSzBkUNtuUGD6scUNjX4w9dIcIQ',
+    'consumer_key'      => 'TtJpZdOIch2fyGygTuOcnwf0F',
+    'consumer_secret'   => '9lVjiTFY32gDCAJQHLFa0mpojb9TgZwaVEyLX7mEFaglZWzzLw',
  
     //
     'url_login'         => 'http://localhost/twitter_login.php',
     //'url_callback'      => 'http://localhost:4200/twitter-callback',
-    'url_callback'      => 'oob'//'http://uat.travooo.com/public/twitter-callback',
+    'url_callback'      => 'http://uat.travooo.com/public/twitter-callback',
 ];
