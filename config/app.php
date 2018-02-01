d8a8505f7e6e34cb124fb8562c04a4ec10c55315
jawad ijaz <jawad.ijaz@granjur.net>
2018-02-01 19:14:18 +0500

    Debug fix

--- config/app.php
+++ config/app.php
@@ -34,7 +34,7 @@ return [
       | application. If disabled, a simple generic error page is shown.
       |
      */
-    'debug' => false,//,env('APP_DEBUG', true),
+    'debug' => env('APP_DEBUG', true),
     /*
       |--------------------------------------------------------------------------
       | Application URL
