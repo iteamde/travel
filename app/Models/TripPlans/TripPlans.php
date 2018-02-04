3b5d1c5f3234ca72d60f97e6f06781122f8fb7c0
Mahmoud Hussien <husen@bezaat.com>
2018-02-04 14:51:30 +0300

    Add TripPlans model

---
+++ app/Models/TripPlans/TripPlans.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\TripPlans;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TripPlans extends Model
+{
+    protected $table = 'trips';
+    
+    public $timestamps = false;
+}
