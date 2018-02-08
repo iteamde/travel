5d96386e6b35fcbc7dfadbfa108ce125b7908f6f
Mahmoud Hussien <husen@bezaat.com>
2018-02-08 15:04:05 +0300

    ISO code field to countries

--- resources/views/backend/place/importresults.blade.php
+++ resources/views/backend/place/importresults.blade.php
@@ -57,7 +57,7 @@
                             <input type="hidden" name="place[{{$i}}][provider_id]" value='{{$r->place_id}}' />
                             <input type="hidden" name="place[{{$i}}][name]" value='{{$r->name}}' />
                             <input type="hidden" name="place[{{$i}}][types]" value='{{@join(',', $r->types)}}' />
-                            <input type="hidden" name="place[{{$i}}][address]" value='{{$r->formatted_address}}' />
+                            <input type="hidden" name="place[{{$i}}][address]" value='{{@$r->formatted_address}}' />
                             <input type="hidden" name="place[{{$i}}][lat]" value='{{$r->geometry->location->lat}}' />
                             <input type="hidden" name="place[{{$i}}][lng]" value='{{$r->geometry->location->lng}}' />
                             <input type="hidden" name="place[{{$i}}][phone]" value='{{@$r->international_phone_number}}' />
