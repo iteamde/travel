e0dc63a6672689fa3ce887f1325808bf121849f5
Mahmoud Hussien <husen@bezaat.com>
2018-03-05 14:51:02 +0300

    modify cors policy

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
     */
    
     'supportsCredentials' => false,
-    'allowedOrigins' => ['*'],
+    'allowedOrigins' => ['18.219.231.17'],
     'allowedOriginsPatterns' => [],
     'allowedHeaders' => ['*'],
     'allowedMethods' => ['*'],
